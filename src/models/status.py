6d311fbb804710b7f422095c79be7643ce4bf88f
Tomas Climente <detomas@gmail.com>
2021-07-05 18:56:24 -0300

    fix: datetime in model

--- src/models/status.py
+++ src/models/status.py
@@ -8,7 +8,7 @@ class StatusModel(BaseModel):
     server: str = Field(...)
     implementation: str = Field(...)
     value: str = Field(...)
-    blame_timestamp: datetime = Field(...)
+    blame_timestamp: datetime = datime.now()
 
     class Config:
         allow_population_by_field_name = True
