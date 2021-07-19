fa8de49d7cf8efac668515dd67450f2e83bb6404
Tomas Climente <detomas@gmail.com>
2021-07-19 12:20:42 -0300

    fix: fix utc time

--- src/models/status.py
+++ src/models/status.py
@@ -8,7 +8,7 @@ class StatusModel(BaseModel):
     server: str = Field(...)
     implementation: str = Field(...)
     value: str = Field(...)
-    blame_timestamp: datetime = Field(default_factory=datetime.utcnow())
+    blame_timestamp: datetime = Field(default_factory=datetime.utc)
 
     class Config:
         allow_population_by_field_name = True
